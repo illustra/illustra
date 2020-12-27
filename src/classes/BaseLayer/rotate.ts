82d107a143820a9ce2604fdcc78a32d175997648
APixelVisuals <apixelvisualsofficial@gmail.com>
2020-12-26 18:30:40 -0800

    Rotating a layer now updates the layers width and height properties

--- src/classes/BaseLayer/rotate.ts
+++ src/classes/BaseLayer/rotate.ts
@@ -5,6 +5,18 @@ export default function rotate<AnyLayerInput extends AnyLayer>(layer: AnyLayerIn
     // Debug
     layer._debug(`Rotating by ${degrees} degrees`);
 
+    // Get width and height
+    let width: number = layer.width;
+    let height: number = layer.height;
+
+    // Get rotated angle in radians
+    const rotatedAngle: number = degrees * (Math.PI / 180);
+
+    // Set width and height
+    // https://stackoverflow.com/a/3231438/8961491
+    layer.width = Math.round(Math.abs(width * Math.cos(rotatedAngle)) + Math.abs(height * Math.sin(rotatedAngle)));
+    layer.height = Math.round(Math.abs(width * Math.sin(rotatedAngle)) + Math.abs(height * Math.cos(rotatedAngle)));
+
     // Add to edits
     layer._edits.push({
         type: "rotate",
